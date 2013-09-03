8759718ccb93747262b6455f4a4ce421a816eb85
Alexander Makarov <makarov.dev@gmail.com>
2013-09-03 22:35:40 +0400

    - test console app reads syncPairs from config file and do sync for all pairs on user input - add ToString method for IFileSystemManager - keep BeaverSync configuration in separate file (beaverSync.xml) inside app work dir - add example of beaverSync.xml

--- BeaverSync/BeaverSyncLib/IFileSystemManager.cs
+++ BeaverSync/BeaverSyncLib/IFileSystemManager.cs
@@ -41,5 +41,10 @@ namespace BeaverSyncLib
         /// Размер файла в байтах
         /// </summary>
         public long ByteSize { get; set; }
+
+        public override string ToString()
+        {
+            return String.Format("LastModified = {0:dd.MM.yyyy HH:mm:ss}, ByteSize = {1}", LastModified, ByteSize);
+        }
     }
 }
